5679fabd04f178fe48c4ebbefffc04f324168af1
Roel van den Berg <roel.vandenberg@kadaster.nl>
2022-07-13 12:36:38 +0200

    Fix example test run with docker-compose

--- example/run_test.sh
+++ example/run_test.sh
@@ -9,11 +9,11 @@ export KUBECONFIG=$SCRIPT_DIR/kubeconfig.yaml
 mkdir -p output
 
 sleep 5
-docker wait $(docker-compose -f $SCRIPT_DIR/docker-compose.yaml ps | grep kubernetes-agent_ | cut -d " " -f1)
-kustomize build $SCRIPT_DIR/kubernetes-config | kubectl apply -f -
+#docker wait $(docker-compose -f $SCRIPT_DIR/docker-compose.yaml ps | grep kubernetes-agent_ | cut -d " " -f1)
+kustomize build $SCRIPT_DIR/goaf | kubectl apply -f -
 
 docker build . -t pdok/ets-ogcapi-features10-docker:test
 
 kubectl wait pods -l app=ogcapi-features --for=condition=Ready --timeout=180s
 
-docker run -v "$(pwd):/mnt"  pdok/ets-ogcapi-features10-docker:test https://localhost:32788/geonovum/oaf/v1_0/ --generateHtmlReport true --outputDir /mnt/output
+docker run --network=example_ogcapi -v "$(pwd):/mnt"  pdok/ets-ogcapi-features10-docker:test http://kubernetes-server/geonovum/oaf/v1_0/ --generateHtmlReport true --outputDir /mnt/output
